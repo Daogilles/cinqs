d5e7a09b80ef4fc92b98fb6960362530b234005d
Gilles Dao Duc <gillesdaoduc@iMac-de-Allan.local>
2015-12-29 18:11:33 +0100

    done page business

--- web/scripts/bundle.js
+++ web/scripts/bundle.js
@@ -218,7 +218,29 @@ function App() {
 
         $('.interne-header .logo').on('click', function() {
             $('.dropdown-logo').hasClass('active') ? $('.dropdown-logo').removeClass('active') : $('.dropdown-logo').addClass('active');
+        });
+
+
+        $('.driver-header .account').on('click', function() {
+            $('.dropdown-logo').hasClass('active') ? $('.dropdown-logo').removeClass('active') : $('.dropdown-logo').addClass('active');
+        });
+
+        $('.datepicker').datepicker({
+            firstDay: 1,
+            altField: "#datepicker",
+            closeText: 'Fermer',
+            prevText: 'Précédent',
+            nextText: 'Suivant',
+            currentText: 'Aujourd\'hui',
+            monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+            monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+            dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            dayNamesShort: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+            dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+            weekHeader: 'Sem.',
+            dateFormat: 'yy-mm-dd'
         })
+        $.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
 
     }; 
 
